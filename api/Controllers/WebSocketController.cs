e432c47e29f3a5f0de65727a58a2d94ddb9f2ec2
Heidi <cedi@hotmail.fi>
2024-03-18 12:46:22 +0200

    first calculation for new coordinate

--- api/Controllers/WebSocketController.cs
+++ api/Controllers/WebSocketController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 using System.Net.WebSockets;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace api.Controllers
 {
@@ -31,10 +34,15 @@ namespace api.Controllers
                 new ArraySegment<byte>(buffer), CancellationToken.None);
 
             Console.WriteLine("New message received : " + Encoding.UTF8.GetString(buffer, 0, receiveResult.Count));
+            Console.WriteLine("keke:" + receiveResult);
+            var jei = NewCoordinates(Encoding.UTF8.GetString(buffer, 0, receiveResult.Count));
             while (!receiveResult.CloseStatus.HasValue)
             {
+                byte[] bytes = Encoding.Default.GetBytes(jei);
+                jei = Encoding.UTF8.GetString(bytes);
                 await webSocket.SendAsync(
-                    new ArraySegment<byte>(buffer, 0, receiveResult.Count),
+                    bytes,
+                    //new ArraySegment<byte>(buffer, 0, receiveResult.Count),
                     receiveResult.MessageType,
                     receiveResult.EndOfMessage,
                     CancellationToken.None
@@ -50,6 +58,25 @@ namespace api.Controllers
                 receiveResult.CloseStatus.Value,
                 receiveResult.CloseStatusDescription,
                 CancellationToken.None);
+        }
+
+        private static string NewCoordinates(string oldCoordinates)
+        {
+            var currentCoordinate = JsonSerializer.Deserialize<Dictionary<string, double>>(oldCoordinates);
+            double r = 0.0017;
+            double pI = Math.PI;
+            double rotaitingAngle = 2 * pI / 12;
+            double newAngle = 2 * pI / 6;
+            Console.WriteLine(currentCoordinate["lat"] + " " + currentCoordinate["lng"] +" "+ rotaitingAngle +" "+ newAngle);
+            Dictionary<string, double> newCoordinate = new Dictionary<string, double>
+            {
+                { "lat", currentCoordinate["lat"] + r * Math.Cos(rotaitingAngle) },
+                { "lng", currentCoordinate["lng"] + r * Math.Sin(rotaitingAngle) }
+            };
+
+            var returnvalue = JsonSerializer.Serialize(newCoordinate);
+            Console.WriteLine("palautettava value: " +returnvalue);
+            return returnvalue;
         }
     }
 }
